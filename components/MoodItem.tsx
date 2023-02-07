1b23e7291646cbf51ad23ddee7297ee466cbc3ad
Andy <andrewfagan585@gmail.com>
2023-02-06 19:32:37 -0500

    tailwind did not handle dynamic classes well

--- components/MoodItem.tsx
+++ components/MoodItem.tsx
@@ -7,7 +7,7 @@ interface IMoodItem {
 
 const MoodItem:React.FC<IMoodItem> = ({color, value}) => {
 
-    const classes = `bg-${color.toLowerCase()}-600 border-${color.toLowerCase()}-700`;
+    const classes = color === 'Green' ? 'bg-green-600 border-green-700' : 'bg-red-600 border-red-700';
 
     return (
         <div className={`border-2 p-2 rounded-full shadow text-center text-white ${classes}`}>
